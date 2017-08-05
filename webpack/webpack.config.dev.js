d11e597de9ecc8ab9b02a0a59c09928925ff340e
Javier Bórquez <hi@javier.xyz>
2017-08-05 03:36:10 -0500

    Update dependencies and screenshot

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -1,27 +1,29 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
+  devtool: "cheap-module-eval-source-map",
   entry: [
-    'eventsource-polyfill',
-    'webpack-hot-middleware/client',
-    './src/index.js'
+    "eventsource-polyfill",
+    "webpack-hot-middleware/client",
+    "./src/index.js"
   ],
   output: {
-    path: path.join(__dirname, '../dist'),
-    filename: 'bundle.js',
-    publicPath: '/dist/'
+    path: path.join(__dirname, "../dist"),
+    filename: "bundle.js",
+    publicPath: "/dist/"
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoEmitOnErrorsPlugin()
   ],
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, '../src')
-    }]
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ["babel-loader"],
+        include: path.join(__dirname, "../src")
+      }
+    ]
   }
 };
--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -1,37 +1,37 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
+const BabiliPlugin = require("babili-webpack-plugin");
 
 module.exports = {
-  devtool: 'source-map',
-  entry: [
-    './src/index'
-  ],
+  devtool: "source-map",
+  entry: ["./src/index"],
   output: {
-    path: path.join(__dirname, '../dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
+    path: path.join(__dirname, "../dist"),
+    filename: "bundle.js",
+    publicPath: "/static/"
   },
   plugins: [
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
       }
     }),
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false
-      },
-      output: {
+    new BabiliPlugin(
+      {},
+      {
+        test: /\.js$/,
         comments: false
       }
-    })
+    )
   ],
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, '../src')
-    }]
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ["babel-loader"],
+        include: path.join(__dirname, "../src")
+      }
+    ]
   }
 };
