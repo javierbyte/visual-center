57b0623aeba1280fc48c76871eb1544627b4658b
Javier Bórquez <hi@javier.xyz>
2019-10-02 03:08:41 -0500

    Updates

---
+++ precache-manifest.2fe276f37a59bcb78759119a4eb2ec83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2644f63f8b47a889bee18044df209c8",
+    "url": "/visual-center/index.html"
+  },
+  {
+    "revision": "8e073c4ae3e0531bb9da",
+    "url": "/visual-center/static/css/main.6326e36b.chunk.css"
+  },
+  {
+    "revision": "d304c35a8d18eb657c10",
+    "url": "/visual-center/static/js/2.e0cfaced.chunk.js"
+  },
+  {
+    "revision": "8e073c4ae3e0531bb9da",
+    "url": "/visual-center/static/js/main.96915335.chunk.js"
+  },
+  {
+    "revision": "08e589b2bc62628fd7f7",
+    "url": "/visual-center/static/js/runtime-main.414b0029.js"
+  }
+]);
